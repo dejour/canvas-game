27b3ac968417345dfc0de4fd5da6b5adc4d766dc
evan Liu <krystalnumber@gmail.com>
2017-01-24 16:03:25 +0800

    first commit

---
+++ js/resources.js
@@ -0,0 +1,63 @@
+(function () {
+	var resourceCache = {};
+	var loading = [];
+	var readyCallbacks = [];
+
+	// Load an image url or an array of image urls
+	function load(urlorArr) {
+		if (urlorArr instanceof Array) {
+			urlorArr.forEach(function (url) {
+				_load(url);
+			})
+		} else {
+			_load(urlorArr);
+		}
+	}
+
+	function _load(url) {
+		if (resourceCache[url]) {
+			return resourceCache[url];
+		} else {
+			var img = new Image();
+			img.onload = function () {
+				resourceCache[url] = img;
+
+				if (isReady()) {
+					readyCallbacks.forEach(function (func) {
+						func();
+					})
+				}
+			};
+
+			resourceCache[url] = false;
+			img.src = url;
+		}
+	}
+
+	function get(url) {
+		return resourceCache[url];
+	}
+
+	function isReady() {
+		var ready = true;
+		for (var k in resourceCache) {
+			if (resourceCache.hasOwnProperty(k) &&
+				!resourceCache[k]) {
+				ready = false
+			}
+		}
+		return ready;
+	}
+
+	function onReady(func) {
+		readyCallbacks.push(func);
+	}
+
+	window.resources = {
+		load: load,
+		get: get,
+		onReady: onReady,
+		isReady: isReady
+	}
+
+})();
